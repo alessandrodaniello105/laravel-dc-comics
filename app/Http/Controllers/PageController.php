b84c14ae58d0aeeef0a70c2d7c8480e27860dca7
alessandrodaniello105 <kanymsevement@gmail.com>
2023-11-23 16:47:29 +0100

    [add]: num_products in home

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -1,12 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Comic;
 
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index() {
-        return view('home');
+        $num_products = Comic::count();
+        return view('home', compact('num_products'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,8 @@
 @section('content')
 
 <div class="container my-3 text-center">
-    <h1>home page</h1>
+    <h1>Home Page Libreria DC</h1>
+    <span>Sono presenti {{$num_products}} fumetti in questa libreria</span>
 </div>
 
 @endsection
