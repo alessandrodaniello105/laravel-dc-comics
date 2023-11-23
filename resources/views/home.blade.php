1fa7021e3ac704beec75e379633f3d1354246140
alessandrodaniello105 <kanymsevement@gmail.com>
2023-11-23 15:32:11 +0100

    [add]: Comic as model/controller+resource+index

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('content')
+
+<div class="container my-3 text-center">
+    <h1>home page</h1>
+</div>
+
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,14 @@
+<div class="container my-3">
+    <header>
+
+        <ul class="nav">
+            <li class="nav-item">
+              <a class="nav-link active" href="{{ route('home') }}">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('comics.index') }}">Fumetti DC</a>
+            </li>
+          </ul>
+
+    </header>
+</div>
