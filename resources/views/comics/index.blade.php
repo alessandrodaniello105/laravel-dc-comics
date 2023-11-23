bccb1e5a660c4b5c127d31adaa3d1c8d0d752d46
alessandrodaniello105 <kanymsevement@gmail.com>
2023-11-23 16:23:37 +0100

    [add]: empty table in comics.index

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -2,6 +2,37 @@
 
 @section('content')
 
-    <h1>Comics</h1>
+<div class="container my-3">
+    <h1>Fumetti venduti</h1>
 
+
+    <table class="table">
+        <thead>
+
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Titolo</th>
+            <th scope="col">Prezzo di vendita</th>
+            <th scope="col">Data di vendita</th>
+          </tr>
+
+        </thead>
+
+        <tbody>
+            @foreach ($comics as $comic)
+            <tr>
+                <td>{{ $comic->title }}</td>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+            </tr>
+            @endforeach
+
+
+        </tbody>
+
+      </table>
+
+
+</div>
 @endsection
